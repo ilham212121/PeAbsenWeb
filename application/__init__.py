516a4e1d13791a3d3056685de35a127911781324
Rizky Dwi Saputra <55983435+rixkyduwi@users.noreply.github.com>
2022-07-07 11:49:04 +0700

    delete other db
    delete other db
    
    Co-Authored-By: ilham212121 <80658667+ilham212121@users.noreply.github.com>

--- application/__init__.py
+++ application/__init__.py
@@ -9,24 +9,10 @@ from sqlalchemy.engine import create_engine
 from sqlalchemy.schema import *
 from pybigquery.api import ApiClient
 app = Flask(__name__)
-db= SQLAlchemy()
 mysql = MySQL()
-APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
-dotenv_path = os.path.join(APP_ROOT, '.env')
-load_dotenv(dotenv_path)
-mongo = os.getenv('mongodb://localhost:27017')
-client = pymongo.MongoClient(mongo)
-dbmongo = client['data'] # Mongo collection
-nama = dbmongo['nama'] # Mongo document
-users = dbmongo['email'] # Mongo document
-password = dbmongo['password'] # Mongo document
-no_rumah = dbmongo['no_rumah'] # Mongo document
-kontak = dbmongo['kontak'] # Mongo document
 UPLOAD_FOLDER = 'application/static/upload/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'bukan rahasia'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///Pe_Absen.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 app.config['MYSQL_HOST']= 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD']  = ''
@@ -36,7 +22,6 @@ app.config.update(dict(
 SECRET_KEY="powerful secretkey",
 WTF_CSRF_SECRET_KEY="wis tak omongi ora rahasia"
     ))
-db.init_app(app)
 mysql.init_app(app)
 from application.models import data
 
--- application/routes.py
+++ application/routes.py
@@ -10,50 +10,21 @@ def index():
 def hello_world():
     ip_addr = request.remote_addr
     return '<h1> Your IP address is:' + ip_addr
-@app.route('/apisqlite',methods=['POST'])
-def apisqlite():
-    nip = request.form['nip']
-    password = request.form['password']
-    datadb = data.query.filter_by(nip=nip).first() 
-    if not datadb:
-        return 'NIP tidak ditemukan'
-    elif not check_password_hash(datadb.password, password):
-        return "password salah"
-    return "login berhasil"
 
 @app.route('/apimysql',methods=['POST'])
 def apimysql():
     data = mysql.connection.cursor()
-    email = request.form['email']
-    nama = request.form['nama']
+    nip = request.form['nip']
     password = request.form['password']
-    no_rumah = request.form['no_rumah']
-    kontak = request.form['kontak']
-    data.execute("SELECT email FROM data WHERE email= %s " , (email,))
+    data.execute("SELECT nip FROM data WHERE nip= %s and password= %s " , (nip,))
     datayangada = data.fetchall()
     if str(datayangada) == '()':
-        data.execute("INSERT INTO data (nama,email,password,no_rumah,kontak) values (%s,%s,%s,%s,%s)",(nama,email,password,no_rumah,kontak))
-        mysql.connection.commit()
-        return "Data Telah Disimpan"
+        return "maaf nip tida ada"
     else:
-        return "maaf email sudah ada"
-@app.route('/apimongo',methods=['POST'])
-def apimongo():
-    email = request.form['email']
-    nama = request.form['nama']
-    password = request.form['password']
-    no_rumah = request.form['no_rumah']
-    kontak = request.form['kontak']    
-    email = users.find_one({"email": email})
-    if email:
-        return 'This email is already exist.'
-    new_data = {
-            'nama':nama,
-            'email': email,
-            'password': password,
-            'no_rumah': no_rumah,
-            'kontak': kontak,
-        }
-    users.insert_one(new_data)
-    return ''+new_data['nama'] + ' user has been added. success'
+        data.execute("SELECT nip FROM data WHERE email= %s and password = %s " , (nip,password))
+        datapswd = data.fetchall()
+        if str(datapswd) == '()':
+            return "maaf password salah"
+        else:
+            return "login berhasil"
     
