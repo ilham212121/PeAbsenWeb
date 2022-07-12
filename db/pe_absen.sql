a3b00e7d4cca03724aaa4d59d9d285294e4e9cae
ilham212121 <80658667+ilham212121@users.noreply.github.com>
2022-07-12 13:26:41 +0700

    drop down ruangan

--- db/pe_absen.sql
+++ db/pe_absen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 08, 2022 at 06:09 AM
+-- Generation Time: Jul 12, 2022 at 08:05 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -24,27 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `data`
+-- Table structure for table `admin`
 --
 
-CREATE TABLE `data` (
+CREATE TABLE `admin` (
   `nip` int(10) DEFAULT NULL,
   `nama` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `no_rumah` varchar(10) NOT NULL,
+  `password` varchar(1000) NOT NULL,
+  `alamat` varchar(1000) NOT NULL,
   `kontak` varchar(13) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `data`
+-- Dumping data for table `admin`
 --
 
-INSERT INTO `data` (`nip`, `nama`, `email`, `password`, `no_rumah`, `kontak`) VALUES
-(NULL, 'RIzky Dwi Saputra', 'rizkyds@gmail.com', 'blabla12', '12', '08921423532'),
-(NULL, 'm ilham fs', 'ilhamdariatas@gmail.com', 'bla123', '123', '089214322351'),
-(NULL, 'm ilham fs', 'ilhamdariat@gmail.com', 'bla123', '123', '089214322351'),
-(19090107, 'Rizky Dwi Saputra', 'rizkydwisaputrar1@gmail.com', 'blabla12', '12', '08953605107');
+INSERT INTO `admin` (`nip`, `nama`, `email`, `password`, `alamat`, `kontak`) VALUES
+(19090107, 'Rizky Dwi Saputra', 'rizkydwisaputrar1@gmail.com', 'sha256$nLb0vObcNS7FiHeC$645acf9b245e3d523e5629b59e3ba5750cb60ce6ee0b1cdab44cb5a07c22b49d', 'jl balamoa selatan rt 03/01', '08953605107'),
+(19090101, 'M Ilham Fajar S', 'ilham@gmail.com', 'pbkdf2:sha256:260000$wZS9dWu7iGiaoHg4$8ba676fd6c10fa74eff20866639e183ff293b879b31cd4111ce62c87c45a54', 'jl sriti gang 3 no 4', '0895365912452');
 
 -- --------------------------------------------------------
 
@@ -55,11 +53,81 @@ INSERT INTO `data` (`nip`, `nama`, `email`, `password`, `no_rumah`, `kontak`) VA
 CREATE TABLE `dataabsen` (
   `nip` varchar(10) DEFAULT NULL,
   `nama` varchar(100) DEFAULT NULL,
-  `waktu` date DEFAULT NULL,
+  `ruangan` varchar(100) NOT NULL,
   `lokasi` geometry DEFAULT NULL,
   `foto` varchar(100) NOT NULL,
-  `sidik jari` text DEFAULT NULL,
+  `waktu` date DEFAULT NULL,
   `status` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dataabsen`
+--
+
+INSERT INTO `dataabsen` (`nip`, `nama`, `ruangan`, `lokasi`, `foto`, `waktu`, `status`) VALUES
+('19090107', 'Rizky Dwi Saputra', 'bougenvile', NULL, 'bio.jpg', '2022-07-12', 'telat'),
+('19090107', 'Rizky Dwi Saputra', 'bougenvile', NULL, 'IMG20220712073116.jpg', '2022-07-12', 'tidak telat'),
+('19090101', 'M Ilham Fajar S', 'dahlia', NULL, 'IMG20220712110028.jpg', '2022-07-12', 'tidak telat'),
+('19090101', 'M Ilham Fajar S', 'dahlia', NULL, 'IMG20220712110028.jpg', '2022-07-12', 'telat'),
+('19090107', 'Rizky Dwi Saputra', 'mawar', NULL, 'IMG20220712083001.jpg', '2022-07-11', 'tidak telat'),
+('19090101', 'M Ilham Fajar S', 'mawar', NULL, 'IMG20220712110028.jpg', '2022-07-11', 'telat'),
+('19090101', 'M ilham Fajar S', 'melati', NULL, 'IMG20220712110028.jpg', '2022-07-13', 'tidak telat'),
+('19090107', 'Rizky Dwi Saputra', 'melati', NULL, 'IMG20220712083001.jpg', '2022-07-13', 'telat'),
+('19090107', 'Rizky Dwi Saputra', 'anggrek', NULL, 'bio.jpg', '2022-07-13', 'telat'),
+('19090101', 'M Ilham Fajar S', 'anggrek', NULL, 'IMG20220712110028.jpg', '2022-07-13', 'tidak telat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hrd`
+--
+
+CREATE TABLE `hrd` (
+  `nip` varchar(10) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `no hp` varchar(15) NOT NULL,
+  `alamat` varchar(1000) NOT NULL,
+  `jabatan` varchar(10) NOT NULL DEFAULT 'HRD'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `nip` varchar(10) NOT NULL,
+  `pswd` varchar(1000) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `posisi` varchar(100) NOT NULL,
+  `gender` varchar(1) NOT NULL,
+  `ttl` date NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `alamat` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `karyawan`
+--
+
+INSERT INTO `karyawan` (`nip`, `pswd`, `nama`, `posisi`, `gender`, `ttl`, `email`, `no_hp`, `alamat`) VALUES
+('220712001', 'pbkdf2:sha256:260000$wZS9dWu7iGiaoHg4$8ba676fd6c10fa74eff20866639e183ff293b879b31cd4111ce62c87c45a54c1', 'Bambang', 'Perawat', 'L', '1992-08-03', 'banbangkeuh@gmail.com', '0895360510704', 'jl jalak barat gang 3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kep_ruang`
+--
+
+CREATE TABLE `kep_ruang` (
+  `nip` varchar(10) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `penempatan` varchar(1000) NOT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `alamat` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 COMMIT;
 
